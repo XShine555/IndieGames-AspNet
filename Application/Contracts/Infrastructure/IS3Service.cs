4399a45d8e5438651a20650de5e6b8b4ca74c769
NotS1mp <xshine555@gmail.com>
2026-04-08 16:04:38 +0200

    Added missing methods

--- Application/Contracts/Infrastructure/IS3Service.cs
+++ Application/Contracts/Infrastructure/IS3Service.cs
@@ -2,5 +2,9 @@
 {
     public interface IS3Service
     {
+        Task UploadFileAsync(Stream fileStream, string keyName, string contentType, CancellationToken cancellationToken);
+        Task RemoveFileAsync(string keyName, CancellationToken cancellationToken);
+        Task<Stream> GetFileStreamAsync(string keyName, CancellationToken cancellationToken);
+        Task<string> GetSignedUrlAsync(string keyName, TimeSpan expiration, CancellationToken cancellationToken);
     }
 }
