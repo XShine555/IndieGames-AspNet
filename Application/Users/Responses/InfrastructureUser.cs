7fbac5c790700c1f540363c8e4b66b64e6c7285c
XShine555 <xshine555@gmail.com>
2026-04-09 01:37:26 +0200

    Fix

--- Application/Users/Responses/InfrastructureUser.cs
+++ Application/Users/Responses/InfrastructureUser.cs
@@ -7,11 +7,8 @@ namespace Application.Users.Responses
     {
         public static InfrastructureUser FromCognitoUser(UserType userType)
         {
-            var username = userType.Attributes.SingleOrDefault(a => a.Name == "username")
-                ?? throw new InvalidOperationException("Cognito user does not have a 'username' attribute.");
-
             return new InfrastructureUser(
-                username.Value);
+                userType.Username);
         }
     }
 }
