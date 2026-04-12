90d6d89edc5dfeae80ce5e1162ff7a6b07fde91f
XShine555 <xshine555@gmail.com>
2026-04-12 16:03:31 +0200

    Move to file

--- Application/Users/Responses/ApplicationUser.cs
+++ Application/Users/Responses/ApplicationUser.cs
@@ -3,42 +3,6 @@ using Domain.Entities;
 
 namespace Application.Users.Responses
 {
-    public record ApplicationUserPicture(
-        int PictureId,
-        string? OriginalName,
-        string? OriginalFileExtension,
-        string? OriginalRelativePath,
-        string SmallRelativePath,
-        string SmallName,
-        string SmallFileExtension,
-        string MediumRelativePath,
-        string MediumName,
-        string MediumFileExtension,
-        string LargeRelativePath,
-        string LargeName,
-        string LargeFileExtension,
-        DateTime AddedAt)
-    {
-        public static ApplicationUserPicture FromEntity(UserProfilePictures profilePicture)
-        {
-            return new ApplicationUserPicture(
-                profilePicture.Id,
-                profilePicture.OriginalName,
-                profilePicture.OriginalFileExtension,
-                profilePicture.OriginalRelativePath,
-                profilePicture.SmallRelativePath,
-                profilePicture.SmallName,
-                profilePicture.SmallFileExtension,
-                profilePicture.MediumRelativePath,
-                profilePicture.MediumName,
-                profilePicture.MediumFileExtension,
-                profilePicture.LargeRelativePath,
-                profilePicture.LargeName,
-                profilePicture.LargeFileExtension,
-                profilePicture.AddedAt);
-        }
-    }
-
     public record ApplicationUser(
         string IdentityId,
         string Username,
@@ -67,7 +31,7 @@ namespace Application.Users.Responses
                 user.IdentityId,
                 user.Username,
                 user.DisplayUsername,
-                user.ProfilePicture is null ? null : ApplicationUserPicture.FromEntity(user.ProfilePicture),
+                ApplicationUserPicture.FromEntity(user.ProfilePicture),
                 createdGames,
                 ownedGames,
                 user.CreatedAt,
---
+++ Application/Users/Responses/ApplicationUserPicture.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+
+namespace Application.Users.Responses
+{
+    public record ApplicationUserPicture(
+        int PictureId,
+        string? OriginalName,
+        string? OriginalFileExtension,
+        string? OriginalRelativePath,
+        string SmallRelativePath,
+        string SmallName,
+        string SmallFileExtension,
+        string MediumRelativePath,
+        string MediumName,
+        string MediumFileExtension,
+        string LargeRelativePath,
+        string LargeName,
+        string LargeFileExtension,
+        DateTime AddedAt)
+    {
+        public static ApplicationUserPicture FromEntity(UserProfilePictures profilePicture)
+        {
+            return new ApplicationUserPicture(
+                profilePicture.Id,
+                profilePicture.OriginalName,
+                profilePicture.OriginalFileExtension,
+                profilePicture.OriginalRelativePath,
+                profilePicture.SmallRelativePath,
+                profilePicture.SmallName,
+                profilePicture.SmallFileExtension,
+                profilePicture.MediumRelativePath,
+                profilePicture.MediumName,
+                profilePicture.MediumFileExtension,
+                profilePicture.LargeRelativePath,
+                profilePicture.LargeName,
+                profilePicture.LargeFileExtension,
+                profilePicture.AddedAt);
+        }
+    }
+}
