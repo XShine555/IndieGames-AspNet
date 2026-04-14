9511950ba8d88bb7c6075d5ea554b0f3dd54c40b
XShine555 <xshine555@gmail.com>
2026-04-14 22:51:03 +0200

    Remove unused code

--- Infrastructure/Persistence/Database.cs
+++ Infrastructure/Persistence/Database.cs
@@ -135,20 +135,6 @@ namespace Infrastructure.Persistence
                 a.Property(x => x.ProcessingError)
                     .HasMaxLength(512);
             } );
-
-            /*modelBuilder.Entity<Genre>().HasData(
-                CreateGenre(1, "Action"),
-                CreateGenre(2, "Adventure"),
-                CreateGenre(3, "RPG"),
-                CreateGenre(4, "Strategy"),
-                CreateGenre(5, "Simulation"),
-                CreateGenre(6, "Sports"),
-                CreateGenre(7, "Puzzle"),
-                CreateGenre(8, "Horror"),
-                CreateGenre(9, "Racing"),
-                CreateGenre(10, "Indie"),
-                CreateGenre(11, "FPS")
-            );*/
         }
     }
 }
