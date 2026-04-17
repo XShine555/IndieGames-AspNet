971e95c3b37b8d1b3c1494d5c3e3a8b73b7d6127
NotS1mp <xshine555@gmail.com>
2026-04-17 18:37:35 +0200

    Add price and discount

--- Application/Users/Mappers/UserMapper.cs
+++ Application/Users/Mappers/UserMapper.cs
@@ -89,6 +89,8 @@ namespace Application.Users.Mappers
                 game.Id,
                 game.Title,
                 game.Description,
+                game.Price,
+                game.Discount,
                 game.Genres.Select(genre => new ApplicationGenre(genre.Id, genre.Name, genre.CreatedAt, genre.UpdatedAt)).ToArray(),
                 game.StorePictures.Select(gameMapper.ToApplicationGamePicture).ToArray(),
                 game.Artworks.Select(gameMapper.ToApplicationGameArtwork).ToArray());
