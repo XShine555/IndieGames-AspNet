1fe21f723c8ea8b015b08a1388e6c33e79e36713
NotS1mp <xshine555@gmail.com>
2026-05-05 16:33:19 +0200

    Get files

---
+++ Application/Games/Builds/Handlers/GetFileListByGameBuildIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Abstractions.Persistence;
+using Application.Games.Builds.Queries;
+using Ardalis.Result;
+using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Games.Builds.Handlers
+{
+    public class GetFileListByGameBuildIdQueryHandler(IDatabase database)
+        : IQueryHandler<GetFileListByGameBuildIdQuery, Result<IReadOnlyList<string> >>
+    {
+        public async ValueTask<Result<IReadOnlyList<string>> > Handle(GetFileListByGameBuildIdQuery query, CancellationToken cancellationToken)
+        {
+            var build = await database.GameBuilds
+                .AsNoTracking()
+                .Include(x => x.Game)
+                .Where(x => x.Id == query.BuildId)
+                .SingleOrDefaultAsync(cancellationToken);
+
+            if (build is null)
+                return Result.NotFound("Build not found");
+
+            if (build.Game.OwnerId != query.UserId)
+                return Result.Unauthorized();
+
+            var files = await database.GameBuildFiles
+                .AsNoTracking()
+                .Where(x => x.GameBuildId == query.BuildId)
+                .Select(x => x.FileRelativePath + x.FileName)
+                .ToListAsync(cancellationToken);
+
+            return Result<IReadOnlyList<string>>.Success(files);
+        }
+    }
+}
---
+++ Application/Games/Builds/Queries/GetFileListByGameBuildIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.Result;
+using Mediator;
+
+namespace Application.Games.Builds.Queries
+{
+    public record GetFileListByGameBuildIdQuery(
+        Guid BuildId,
+        Guid UserId)
+        : IQuery<Result<IReadOnlyList<string>> >;
+}
